6a718be85d43b2912ee1832daf6c6f2b752536dc
JangMinKyung <github ebni97@naver.com>
2019-01-26 16:52:11 +0900

    9강

--- blog/models.py
+++ blog/models.py
@@ -10,8 +10,14 @@ def lnglat_validator(value):
         raise ValidationError('Invalid LngLat Type')
 
 class Post(models.Model):
+    STATUS_CHOICES = (
+        ('d', 'Draft'),
+        ('p', 'Published'),
+        ('w', 'Withdrawn'),
+    )
     author = models.CharField(max_length=20)
-    name = models.CharField(max_length=100, verbose_name='제목', help_text='제목을 입력해주세요. 최대 100자')
+    title = models.CharField(max_length=100, verbose_name='제목',
+                            help_text='제목을 입력해주세요. 최대 100자')
     content = models.TextField(verbose_name='내용')
     tags = models.CharField(max_length=100, blank=True)
     lnglat = models.CharField(max_length=50,
@@ -19,6 +25,13 @@ class Post(models.Model):
                               blank=True,
                               validators=[lnglat_validator]
                               )
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    class Meta:
+        ordering = ['-id']
+
+    def __str__(self):
+        return self.title
+
 
