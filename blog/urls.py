d34e2a00580c1e04f071126e63cc8410854b366e
JangMinKyung <github ebni97@naver.com>
2019-01-26 17:11:56 +0900

    10강

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from django.urls import re_path
-
 from . import views
 #현재 폴더에 있는 views를 임포트한다
 
 urlpatterns = [
     re_path(r'^$', views.post_list),
+    re_path(r'^(?P<id>\d+)/$', views.post_detail),
 
 ]
