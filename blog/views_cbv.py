31cf0017d8332ea2b43791c11ed89796be32de94
JangMinKyung <github ebni97@naver.com>
2019-02-05 20:46:35 +0900

    31강

--- blog/views_cbv.py
+++ blog/views_cbv.py
@@ -1,14 +1,12 @@
-from django.views.generic import CreateView
+from django.views.generic import ListView, DetailView, UpdateView, DeleteView, CreateView
 from django import forms
 from.models import Post
 
-class PostForm(forms.ModelForm):
-    class Meta:
-        model = Post
-        fields = '__all__'
+post_list = ListView.as_view(model=Post, paginate_by=10)
+post_detail = DetailView.as_view(model=Post, pk_url_kwarg='id')
 
-class PostCreateView(CreateView):
-    model = Post
-    form_class = PostForm
+post_new = CreateView.as_view(model=Post)
 
-post_new = PostCreateView.as_view()
+post_edit = UpdateView.as_view(model=Post, fields='__all__')
+
+post_delete = DeleteView.as_view(model=Post, success_url='/blog/')
